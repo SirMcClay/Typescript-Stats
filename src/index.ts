de8129e21f51930b7d784d2913b6bcdf1b273e86
sirmcclay <mcclay.design@gmail.com>
2020-11-23 10:04:09 -0300

    feat: Implement static method in MatchReader for simplify things

--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,12 @@ import { Summary } from './Summary';
 import { HtmlReport } from './reportTargets/HtmlReport';
 
 // Create an object that satisfies the 'DataReader' interface
-const csvFileReader = new CsvFileReader('football.csv');
+// const csvFileReader = new CsvFileReader('football.csv');
 
 // Create an instance of MatchReader and pass in something satisfying
 // the 'DataReader' interface
-const matchReader = new MatchReader(csvFileReader);
+// const matchReader = new MatchReader(csvFileReader);
+const matchReader = MatchReader.fromCsv('football.csv');
 matchReader.load();
 
 // const analyzer = new WinsAnalysis('Man United');
